9118281fc3f69900b1fb270f477b00d2905942f8
AlexeyKumin <a.kumin@mail.ru>
2019-03-27 23:45:59 +0300

    add

---
+++ Prob_Th_Shooters/Prob_Th_Shooters/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Prob_Th_Shooters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columin2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(71, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "кол-во выстрелов совершаемых 3мя стрелками";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(442, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Columin2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(25, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(448, 333);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "y[i]";
+            this.Column1.Name = "Column1";
+            // 
+            // Columin2
+            // 
+            this.Columin2.HeaderText = "Sh1";
+            this.Columin2.Name = "Columin2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Sh2";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Sh3";
+            this.Column4.Name = "Column4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(612, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ответ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(235, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "сколько раз провели эксперимент";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(71, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "100";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dataGridView2.Location = new System.Drawing.Point(479, 121);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(554, 333);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "y[i]";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "n[i]";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "n[i] / N";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(725, 36);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(158, 21);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "вероятности равны";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column5,
+            this.Column6});
+            this.dataGridView3.Location = new System.Drawing.Point(1039, 121);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(446, 333);
+            this.dataGridView3.TabIndex = 9;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "p1[i]";
+            this.Column2.Name = "Column2";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "p2[i]";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "p3[i]";
+            this.Column6.Name = "Column6";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(725, 63);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(167, 21);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "задать с клавиатуры";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(176, 475);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(160, 21);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "выключить таблицу";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1127, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 52);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "input";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1497, 543);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columin2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Prob_Th_Shooters/Prob_Th_Shooters/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prob_Th_Shooters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Task
+        {
+            public int n;
+            public int n1;
+            public double[] p1;
+            public double[] p2;
+            public double[] p3;
+
+            public int[] Sh1;
+            public int[] Sh2;
+            public int[] Sh3;
+
+            public int[] y; //Требуемая случайная величина
+
+            public int[] nf; 
+
+            public void Prob()
+            {
+                p1 = new double[n];
+                p2 = new double[n];
+                p3 = new double[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    p1[i] = 1.0 / Convert.ToDouble(n);
+                    p2[i] = 1.0 / Convert.ToDouble(n);
+                    p3[i] = 1.0 / Convert.ToDouble(n);
+                }
+            }
+
+            public void Exper()
+            {
+                Sh1 = new int[n1];
+                Sh2 = new int[n1];
+                Sh3 = new int[n1];
+                y = new int[n1];
+
+                Random rand = new Random();
+                for (int i = 0; i < n1; i++)
+                {
+                    Sh1[i] = Shot(p1, rand);
+                    Sh2[i] = Shot(p2, rand);
+                    Sh3[i] = Shot(p3, rand);
+                    y[i] = Sh1[i] + Sh2[i] + Sh3[i];
+                }
+            }
+
+            public void Frequency()
+            {
+                nf = new int[3 * n + 1];
+                for (int i = 3; i < 3 * n + 1; i++)
+                {
+                    for (int j = 0; j < n1; j++)
+                    {
+                        if (y[j] == i)
+                        {
+                            nf[i] += 1;
+                        }
+                    }
+                }
+            }
+
+            public int Shot(double[] p, Random rand)
+            {
+                int k = 0;
+                int i = 0;
+                while ((k == 0) && (i < n))
+                {
+                    double r = rand.NextDouble();
+                    if (r < p[i])
+                        k = i + 1;
+                    i++;
+                }
+                if (i == n)
+                    k = i;
+                return k;
+            }
+     
+            public Task(int N, int N1)
+            {
+                n = N;
+                n1 = N1;
+                Prob();
+
+                Exper();
+
+                Frequency();
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = Convert.ToInt32(textBox1.Text);
+            int N1 = Convert.ToInt32(textBox2.Text);
+            Task T = new Task(N, N1);
+
+            dataGridView3.Rows.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                dataGridView3.Rows.Add();
+                dataGridView3.Rows[i].Cells[0].Value = T.p1[i];
+                dataGridView3.Rows[i].Cells[1].Value = T.p2[i];
+                dataGridView3.Rows[i].Cells[2].Value = T.p3[i];
+            }
+
+            if (radioButton1.Checked == true)
+            {
+                if (checkBox1.Checked == false)
+                {
+                    dataGridView1.Rows.Clear();
+                    for (int i = 0; i < N1; i++)
+                    {
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[i].Cells[0].Value = T.y[i];
+                        dataGridView1.Rows[i].Cells[1].Value = T.Sh1[i];
+                        dataGridView1.Rows[i].Cells[2].Value = T.Sh2[i];
+                        dataGridView1.Rows[i].Cells[3].Value = T.Sh3[i];
+                    }
+                }
+
+                dataGridView2.Rows.Clear();
+                double norm = 0;
+                for (int i = 3; i < 3 * N + 1; i++)
+                {
+                    dataGridView2.Rows.Add();
+                    dataGridView2.Rows[i - 3].Cells[0].Value = i;
+                    dataGridView2.Rows[i - 3].Cells[1].Value = T.nf[i];
+                    dataGridView2.Rows[i - 3].Cells[2].Value = Convert.ToDouble(T.nf[i]) / Convert.ToDouble(N1);
+                    norm += Convert.ToDouble(T.nf[i]) / Convert.ToDouble(N1);
+                }
+                
+                label2.Text = Convert.ToString(norm);
+            }
+
+            if (radioButton2.Checked == true)
+            {
+                button2.Visible = true;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int N = Convert.ToInt32(textBox1.Text);
+            int N1 = Convert.ToInt32(textBox2.Text);
+            Task T = new Task(N, N1);
+
+            for (int i = 0; i < T.n; i++)
+            {
+                T.p1[i] = Convert.ToDouble(dataGridView3.Rows[i].Cells[0].Value);
+                T.p2[i] = Convert.ToDouble(dataGridView3.Rows[i].Cells[1].Value);
+                T.p3[i] = Convert.ToDouble(dataGridView3.Rows[i].Cells[2].Value);
+            }
+
+            T.Exper();
+            T.Frequency();
+
+            if (checkBox1.Checked == false)
+            {
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < N1; i++)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[0].Value = T.y[i];
+                    dataGridView1.Rows[i].Cells[1].Value = T.Sh1[i];
+                    dataGridView1.Rows[i].Cells[2].Value = T.Sh2[i];
+                    dataGridView1.Rows[i].Cells[3].Value = T.Sh3[i];
+                }
+            }
+
+            dataGridView2.Rows.Clear();
+            double norm = 0;
+            for (int i = 3; i < 3 * N + 1; i++)
+            {
+                dataGridView2.Rows.Add();
+                dataGridView2.Rows[i - 3].Cells[0].Value = i;
+                dataGridView2.Rows[i - 3].Cells[1].Value = T.nf[i];
+                dataGridView2.Rows[i - 3].Cells[2].Value = Convert.ToDouble(T.nf[i]) / Convert.ToDouble(N1);
+                norm += Convert.ToDouble(T.nf[i]) / Convert.ToDouble(N1);
+            }
+
+            label2.Text = Convert.ToString(norm);
+
+        }
+    }
+}
